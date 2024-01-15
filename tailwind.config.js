7ef293df3a7c03f96917f9c5e4a1f756078053c6
antony0740 <antonywasonga21@gmail.com>
2024-01-15 23:54:28 +0300

    update 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,15 @@ module.exports = {
     },
     extend: {
       colors:{
-        brightRed: 'hsl(12, 88%, 59%)'
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight:'hsl(12, 88%, 69%)',
+        brightResSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue:'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(223, 12%, 13%)',
+        vareyPaleRed: 'hsl(13, 100%, 96%)',
+        brightRed: 'hsl(0, 0%, 98%)',
+        
       }
     },
   },
